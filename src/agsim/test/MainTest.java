8b0f5d9e128e14acfa00b3ab40c34e30cbe04542
paolokoelio <p.burda@tue.nl>
2018-09-07 11:19:39 +0200

    Refactoring (decoupling)

--- src/agsim/test/MainTest.java
+++ src/agsim/test/MainTest.java
@@ -1,26 +1,30 @@
 package agsim.test;
+
 import javax.xml.parsers.ParserConfigurationException;
 
 import agsim.model.BayesianAdapter;
 import agsim.utils.FileUtils;
 import agsim.utils.ImportAG;
+import agsim.utils.ParseAG;
 
 public class MainTest {
 
 	public static void main(String[] args) {
 		try {
 			ImportAG bs = new ImportAG();
+
 			FileUtils fl = new FileUtils();
 			fl.readFile("files/AttackGraph.xml");
-			BayesianAdapter adapter = new BayesianAdapter();
-			
-			bs.setFile(fl);
-			bs.parseAG();
-			adapter.convertAG(bs.getNodes(), bs.getEdges());
 
-			
+			bs.setFile(fl);
+			bs.importAG();
+			ParseAG ps = new ParseAG(bs.getNodes(), bs.getEdges());
+			ps.parseAG();
+			BayesianAdapter adapter = new BayesianAdapter();
+			adapter.convertAG(ps.getMyNodes(), ps.getMyEdges());
+
 		} catch (ParserConfigurationException e) {
-			
+
 			e.printStackTrace();
 		}
 
---
+++ src/agsim/utils/ParseAG.java
@@ -0,0 +1,70 @@
+package agsim.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class ParseAG {
+
+	private NodeList edges;
+	private NodeList nodes;
+	private List<HashMap<String, String>> myNodes;
+	private List<HashMap<String, String>> myEdges;
+
+	public ParseAG(NodeList nodes, NodeList edges) {
+		this.edges = edges;
+		this.nodes = nodes;
+
+	}
+
+	public void parseAG() {
+
+		this.myNodes = new ArrayList<HashMap<String, String>>();
+		parseElements(nodes, this.myNodes);
+		this.myEdges = new ArrayList<HashMap<String, String>>();
+		parseElements(edges, this.myEdges);
+		System.out.println(this.myNodes);
+		System.out.println(this.myEdges);
+	}
+
+	public List<HashMap<String, String>> getMyNodes() {
+		return myNodes;
+	}
+
+	public List<HashMap<String, String>> getMyEdges() {
+		return myEdges;
+	}
+
+	/**
+	 * @param nodes
+	 */
+	public void parseElements(NodeList xmlNodes, List<HashMap<String, String>> myElements) {
+		for (int temp = 0; temp < xmlNodes.getLength(); temp++) {
+			Node nNode = xmlNodes.item(temp);
+
+			HashMap<String, String> myNode = new HashMap<String, String>();
+
+			NodeList subNodeList = nNode.getChildNodes();
+
+			for (int i = 0; i < subNodeList.getLength(); i++) {
+				Node currentNode = subNodeList.item(i);
+
+				if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) currentNode;
+					myNode.put(eElement.getNodeName(), eElement.getTextContent());
+//						debug
+//						System.out.println(eElement.getNodeName() + ": " + eElement.getTextContent());
+				}
+
+			}
+
+			myElements.add(myNode);
+		}
+
+	}
+
+}
